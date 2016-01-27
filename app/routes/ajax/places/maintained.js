d888e0248bd000687f7bd7610abe7170854efcbc
Vavooon <vavooon@gmail.com>
2016-01-27 13:48:42 +0200

    Move Edit to sidebar

--- app/routes/ajax/places/maintained.js
+++ app/routes/ajax/places/maintained.js
@@ -1,7 +1,7 @@
 module.exports = function(mongoose, placeManager){
 	return function(req, res){
 		if (req.session.user) {
-			placeManager.find({maintainer: mongoose.Types.ObjectId(req.session.user._id)}).populate('maintainer', 'name').exec(function (err, places) {
+			placeManager.find({isConfirmed: true, maintainer: mongoose.Types.ObjectId(req.session.user._id)}).populate('maintainer', 'name').exec(function (err, places) {
 				if (!err) {
 					res.send(JSON.stringify(places));
 				}
