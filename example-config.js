e286d936b40da843e10c5f9802dbe5134fbd0627
Vavooon <vavooon@gmail.com>
2016-03-20 14:07:45 +0100

    Update example config

--- example-config.js
+++ example-config.js
@@ -1,8 +1,9 @@
-var config = {
-		port: 80
+module.exports = {
+	port: 8000
 	,	hostname: '0.0.0.0'
 	, sitename: 'OpenDoor.ooo'
-	,	url: 'http://192.168.100.4:8000'
+	,	url: 'http://192.168.100.2:8000'
+	, prerenderServiceUrl: 'http://localhost:3000/'
 	,	cookieKeys: '1223'
 	,	sessionSecret: '1234567890QWERTY1223'
 	, mongoURI: 'mongodb://localhost:27017/opendoor'
@@ -14,21 +15,30 @@ var config = {
 		//	,	pass: 'userpass'
 		//}
 		//,	senderAddress: 'user@gmail.com'
-			transport: 'smtp'
+		transport: 'smtp'
 		,	host: 'localhost'
 		,	port: 25
 		,	auth: {
-				user: 'username'
+			user: 'username'
 			,	pass: 'password'
-			}
 		}
+	}
 	,	apiKeys: {
-			googleMaps: 'yourApiKey'
+		googleMaps: 'yourApiKey'
 		,	googleMapsServer: 'yourApiKey'
-		}
+		, stripePublic: 'yourApiKey'
+		, stripeSecret: 'yourApiKey'
+	}
 	, social: {
-		twitterAccount: 'yourAccount'
+		twitterAccount: '@yourAccount'
+	}
+	, frontend: {
+		itemsPerPage: 25
+		, maxItemsPerPage: 100
+	}
+	, l10n: {
+		timeFormat: 'hh:mm tt'
+		, dateFormat: 'yyyy-MM-dd'
+		, dateTimeFormat: 'yyyy-MM-dd hh:mm tt'
 	}
 };
-
-module.exports = config;
