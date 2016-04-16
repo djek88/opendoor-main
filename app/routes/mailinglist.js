f109f35ebe5b19f35ea1f3239dc8184138b5acb8
Vavooon <vavooon@gmail.com>
2016-04-16 15:45:21 +0200

    Improve error processing in mailing list generation

--- app/routes/mailinglist.js
+++ app/routes/mailinglist.js
@@ -34,7 +34,10 @@ module.exports = function(subscriptionManager, sm, config, fs, path) {
 			subscriptionManager.find().populate('place', {name: 1, uri: 1, religion: 1, groupName: 1}).exec(function(err, subscriptions){
 
 				json2csv({ data: subscriptions, fields: fields }, function(err, csv) {
-					if (err) console.log(err);
+					if (err) {
+						console.log(err);
+						res.redirect('/error');
+					}
 					res.header('Content-Type', 'text/csv');
 					res.header('Content-Disposition', 'attachment; filename=mailing_list.csv');
 					res.send(csv);
