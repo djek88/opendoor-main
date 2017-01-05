3b464174e82c750956c897edf84fa9c46b5ae206
Ali S.Veliyev <aliveliyev93@gmail.com>
2017-01-06 00:19:38 +0300

    Mouseflow script added

---
+++ assets/js/mouseflow.js
@@ -0,0 +1,7 @@
+var _mfq = _mfq || [];
+(function() {
+    var mf = document.createElement("script");
+    mf.type = "text/javascript"; mf.async = true;
+    mf.src = "//cdn.mouseflow.com/projects/93f24383-78f2-4ce8-8f65-222b871a0198.js";
+    document.getElementsByTagName("head")[0].appendChild(mf);
+})();
