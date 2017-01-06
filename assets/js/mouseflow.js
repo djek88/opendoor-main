5f2008f474c8a16c827b3a56d76bf3b5b747ddd2
Ali S.Veliyev <aliveliyev93@gmail.com>
2017-01-06 20:22:09 +0300

    zoho added

--- assets/js/mouseflow.js
+++ assets/js/mouseflow.js
@@ -5,3 +5,6 @@ var _mfq = _mfq || [];
     mf.src = "//cdn.mouseflow.com/projects/93f24383-78f2-4ce8-8f65-222b871a0198.js";
     document.getElementsByTagName("head")[0].appendChild(mf);
 })();
+var $zoho= $zoho || {salesiq:{values:{},ready:function(){}}};var d=document;s=d.createElement("script");s.type="text/javascript";
+s.defer=true;s.src="https://salesiq.zoho.com/nestino/float.ls?embedname=nestino";
+t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);
