e17be41243ce6914f1912bd825213c6de5340d08
Ali S.Veliyev <aliveliyev93@gmail.com>
2017-01-07 18:22:56 +0300

    mouseflow replace

--- assets/js/mouseflow.js
+++ assets/js/mouseflow.js
@@ -1,7 +1,13 @@
-var _mfq = _mfq || [];
+/*var _mfq = _mfq || [];
 (function() {
     var mf = document.createElement("script");
     mf.type = "text/javascript"; mf.async = true;
     mf.src = "//cdn.mouseflow.com/projects/93f24383-78f2-4ce8-8f65-222b871a0198.js";
     document.getElementsByTagName("head")[0].appendChild(mf);
+})();*/
+window.__insp = window.__insp || [];
+__insp.push(['wid', 298518304]);
+(function() {
+function ldinsp(){if(typeof window.__inspld != "undefined") return; window.__inspld = 1; var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = "inspsync"; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js'; var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); };
+setTimeout(ldinsp, 500); document.readyState != "complete" ? (window.attachEvent ? window.attachEvent('onload', ldinsp) : window.addEventListener('load', ldinsp, false)) : ldinsp();
 })();
